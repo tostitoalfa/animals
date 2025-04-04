b566eed3e0ef27d738b6006e9732ff8a6a6b5d25
tostitoalfa <andrespdiaz1978@gmail.com>
2025-04-03 18:02:50 -0700

    agregado el componente animals show y su archivo css para los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react"
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import AnimalsShow from "./AnimalsShow"
 
+function getRandomAnimal(){
+  const animals = ['cat', 'bird', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+console.log(getRandomAnimal())
+
+function App() {
+
+  const [animals, setAnimals] = useState ([])
+
+  const handleClick =  () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderAnimals =  animals.map((animal, index) => {
+    return <AnimalsShow type={animal} key={index}/> 
+
+  })
   return (
-    <>
-    <h1> Animals Apps</h1>
-      
-      
-    </>
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animals-list">
+        {renderAnimals}
+      </div>
+    </div>
   )
 }
+
+
+
 
 export default App
